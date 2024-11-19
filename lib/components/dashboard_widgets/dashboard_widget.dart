e966e5f2f21d37a93764b6d754e35707676c2809
dhruv282 <dhruvpatel282.dp@gmail.com>
2024-11-18 21:35:06 -0500

    update dashboard widget abstract class

--- lib/components/dashboard_widgets/dashboard_widget.dart
+++ lib/components/dashboard_widgets/dashboard_widget.dart
@@ -4,5 +4,30 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 abstract class DashboardWidget {
   Widget getConfigPage();
 
-  StaggeredGridTile getWidgetTile();
+  Widget getWidget();
+
+  StaggeredGridTile getWidgetTile() {
+    return StaggeredGridTile.count(
+      crossAxisCellCount: 1,
+      mainAxisCellCount: 1,
+      child: WidgetTileWrapper(child: getWidget()),
+    );
+  }
+}
+
+class WidgetTileWrapper extends StatefulWidget {
+  final Widget child;
+  const WidgetTileWrapper({super.key, required this.child});
+
+  @override
+  State<StatefulWidget> createState() => _WidgetTileWrapperState();
+}
+
+class _WidgetTileWrapperState extends State<WidgetTileWrapper> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: widget.child,
+    );
+  }
 }
