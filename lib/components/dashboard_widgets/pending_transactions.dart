f09bec9583d1cbfa05462b7939b7ee3cdc9d617b
dhruv282 <dhruvpatel282.dp@gmail.com>
2025-06-04 16:36:30 -0400

    Move expense list tile to reusable component

--- lib/components/dashboard_widgets/pending_transactions.dart
+++ lib/components/dashboard_widgets/pending_transactions.dart
@@ -1,9 +1,9 @@
 import 'package:expense_manager/components/dashboard_widgets/dashboard_widget.dart';
+import 'package:expense_manager/components/expense/expense_list_tile.dart';
 import 'package:expense_manager/providers/expense_provider.dart';
 import 'package:expense_manager/utils/logger/logger.dart';
 import 'package:expense_manager/utils/snackbar/snackbar.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class PendingTransactions extends DashboardWidget {
@@ -50,85 +50,53 @@ class _PendingTransactionsWidgetState extends State<PendingTransactionsWidget> {
           Expanded(
               child: ListView(
             children: expenseProvider.pendingTransactions.entries
-                .map((m) => m.value.map((e) => Card(
-                      child: ListTile(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                            side: BorderSide(
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .primary
-                                  .withAlpha(75),
-                              width: 1,
-                            )),
-                        title: Text(
-                          e.category,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(e.description),
-                              Text(
-                                DateFormat.yMMMd().format(e.date),
-                                style: TextStyle(
-                                    color:
-                                        Theme.of(context).colorScheme.primary),
+                .map((m) => m.value.map((e) => ExpenseListTile(
+                      expense: e,
+                      onTap: () => showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                            title: Text('Pending Transaction'),
+                            content:
+                                Text('Add this transaction to your records?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  expenseProvider
+                                      .triggerRecurringScheduleRule(
+                                          m.key, e, true)
+                                      .then((_) {
+                                    if (!context.mounted) return;
+                                    Navigator.of(context).pop();
+                                  }).catchError((e) {
+                                    logger.e(e);
+                                  });
+                                },
+                                child: const Text('Skip'),
                               ),
-                            ]),
-                        trailing: Text("\$${e.cost.toStringAsFixed(2)}",
-                            style: TextStyle(
-                              color:
-                                  expenseProvider.getCategoryColor(e.category),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            )),
-                        onTap: () => showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                              title: Text('Pending Transaction'),
-                              content:
-                                  Text('Add this transaction to your records?'),
-                              actions: [
-                                TextButton(
+                              FilledButton(
                                   onPressed: () {
                                     expenseProvider
                                         .triggerRecurringScheduleRule(
-                                            m.key, e, true)
-                                        .then((_) {
+                                            m.key, e, false)
+                                        .then((v) {
                                       if (!context.mounted) return;
                                       Navigator.of(context).pop();
+                                      showSnackBar(
+                                        context,
+                                        'Transaction added successfully!',
+                                        SnackBarColor.success,
+                                      );
                                     }).catchError((e) {
                                       logger.e(e);
+                                      if (!context.mounted) return;
+                                      showSnackBar(
+                                          context,
+                                          'Failed to add transaction',
+                                          SnackBarColor.error);
                                     });
                                   },
-                                  child: const Text('Skip'),
-                                ),
-                                FilledButton(
-                                    onPressed: () {
-                                      expenseProvider
-                                          .triggerRecurringScheduleRule(
-                                              m.key, e, false)
-                                          .then((v) {
-                                        if (!context.mounted) return;
-                                        Navigator.of(context).pop();
-                                        showSnackBar(
-                                          context,
-                                          'Transaction added successfully!',
-                                          SnackBarColor.success,
-                                        );
-                                      }).catchError((e) {
-                                        logger.e(e);
-                                        if (!context.mounted) return;
-                                        showSnackBar(
-                                            context,
-                                            'Failed to add transaction',
-                                            SnackBarColor.error);
-                                      });
-                                    },
-                                    child: Text('Add')),
-                              ]),
-                        ),
+                                  child: Text('Add')),
+                            ]),
                       ),
                     )))
                 .expand((w) => w)
