3f6d686ac82985f3b1b4a656e33e6101a004729f
dhruv282 <dhruvpatel282.dp@gmail.com>
2025-06-08 20:43:30 -0400

    Added delete function to expense edit page

--- lib/pages/edit_expense.dart
+++ lib/pages/edit_expense.dart
@@ -3,6 +3,7 @@ import 'package:expense_manager/components/expense_form/constants.dart';
 import 'package:expense_manager/data/expense_data.dart';
 import 'package:expense_manager/data/recurring_schedule.dart';
 import 'package:expense_manager/providers/expense_provider.dart';
+import 'package:expense_manager/utils/logger/logger.dart';
 import 'package:expense_manager/utils/snackbar/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -41,9 +42,53 @@ class _EditExpensePageState extends State<EditExpensePage> {
   Widget build(BuildContext context) {
     final expenseProvider = Provider.of<ExpenseProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Edit Expense"),
-      ),
+      appBar: AppBar(title: const Text("Edit Expense"), actions: [
+        IconButton(
+          icon: Icon(
+            Icons.delete,
+            color: Colors.redAccent,
+          ),
+          onPressed: () => showDialog(
+            context: context,
+            barrierDismissible: true,
+            builder: (context) => AlertDialog(
+                title: const Text('Delete Expense'),
+                content:
+                    const Text('Are you sure you want to delete this expense?'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text('Cancel'),
+                  ),
+                  FilledButton(
+                    onPressed: () {
+                      expenseProvider.deleteExpense(widget.expense).then((_) {
+                        if (!context.mounted) return;
+                        showSnackBar(
+                          context,
+                          'Expense deleted!',
+                          SnackBarColor.success,
+                        );
+                        Navigator.of(context).pop();
+                      }).catchError((e) {
+                        logger.e(e);
+                        if (!context.mounted) return;
+                        showSnackBar(
+                          context,
+                          'Failed to delete expense :(',
+                          SnackBarColor.error,
+                        );
+                      }).whenComplete(() {
+                        if (!context.mounted) return;
+                        Navigator.of(context).pop();
+                      });
+                    },
+                    child: Text('Delete'),
+                  ),
+                ]),
+          ),
+        )
+      ]),
       body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: ExpenseForm(
